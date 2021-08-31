e5bacf682dafe5efd26343774b8df094b2309844
Andy Hsieh <shoukoo.koike@gmail.com>
2021-08-31 21:30:29 +1000

    Upadte neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,45 +1,41 @@
 local install_path = "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  echo "I'm here"
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 
-
-local use = require('packer').use
-require('packer').startup(function()
-  use('wbthomason/packer.nvim')
-  use('tpope/vim-fugitive')
-  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('neovim/nvim-lspconfig')
-  use('itchyny/lightline.vim')
-  use('hrsh7th/nvim-compe')
-  use('L3MON4D3/LuaSnip')
-  use('kristijanhusak/orgmode.nvim')
-  use('shoukoo/stylua.nvim')
-  use('shoukoo/mei.nvim')
-  use('shoukoo/commentary.nvim')
-  use({
-    'nvim-telescope/telescope.nvim',
-    requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } },
-  })
+local use = require("packer").use
+require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use("tpope/vim-fugitive")
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("neovim/nvim-lspconfig")
+	use("itchyny/lightline.vim")
+	use("hrsh7th/nvim-compe")
+	use("L3MON4D3/LuaSnip")
+	use("kristijanhusak/orgmode.nvim")
+	use("shoukoo/stylua.nvim")
+	use("shoukoo/mei.nvim")
+	use("shoukoo/commentary.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+	})
 end)
 
--- Starting up Stylua
-require('stylua').startup()
 -- Color scheme
-require('mei')
+require("mei")
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 -- Enable number
 vim.wo.number = true
 vim.wo.relativenumber = true
 -- Turn off swapfile
 vim.o.swapfile = false
 -- Share the systemclipboard
-vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
+vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
 -- Save undo history
 vim.cmd([[set undofile]])
 -- Set split
@@ -48,80 +44,80 @@ vim.cmd([[set splitright]])
 -- Ignore cases
 vim.g.ignorecase = true
 -- Remap leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 --Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
 --Enable break indent
 vim.o.breakindent = true
 -- terminal mode
-vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], {})
+vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], {})
 --Set statusbar
 vim.g.lightline = {
-  active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
-  component_function = { gitbranch = 'fugitive#head' },
+	active = { left = { { "mode", "paste" }, { "gitbranch", "readonly", "filename", "modified" } } },
+	component_function = { gitbranch = "fugitive#head" },
 }
 
 -- Filetype indentation
 vim.api.nvim_exec(
-  [[
+	[[
   augroup FiletypeDetect
     autocmd FileType lua setlocal expandtab shiftwidth=2 tabstop=2
   augroup end
 ]],
-  false
+	false
 )
 
 ---------------------------------------------------------------------
 -- LSP Clients
 ---------------------------------------------------------------------
-local nvim_lsp = require('lspconfig')
+local nvim_lsp = require("lspconfig")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(lsp)
-  return function(_, bufnr)
-    local function buf_set_keymap(...)
-      vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+	return function(_, bufnr)
+		local function buf_set_keymap(...)
+			vim.api.nvim_buf_set_keymap(bufnr, ...)
+		end
 
-    -- Mappings.
-    local opts = { noremap = true, silent = true }
+		-- Mappings.
+		local opts = { noremap = true, silent = true }
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    if lsp == 'sumneko_lua' then
-      buf_set_keymap('n', 'gf', [[<Cmd>lua require"stylua".format_file()<CR>]], opts)
-    elseif lsp == 'gopls' then
-      buf_set_keymap('n', 'gf', [[<Cmd>lua Gopls(1000)<CR>]], opts)
-    else
-      buf_set_keymap('n', 'gf', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    end
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		if lsp == "sumneko_lua" then
+			buf_set_keymap("n", "gf", [[<Cmd>lua require"stylua".format_file()<CR>]], opts)
+		elseif lsp == "gopls" then
+			buf_set_keymap("n", "gf", [[<Cmd>lua Gopls(1000)<CR>]], opts)
+		else
+			buf_set_keymap("n", "gf", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+		end
 
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  end
+		buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+		buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+		buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+		buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+		buf_set_keymap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+		buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+		buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+		buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+		buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+		buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+		buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+		buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+		buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+		buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+		buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+		buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+	end
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'gopls', 'tsserver' }
+local servers = { "gopls", "tsserver" }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup({ on_attach = on_attach(lsp) })
+	nvim_lsp[lsp].setup({ on_attach = on_attach(lsp) })
 end
 
 -- Instruction: https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
@@ -137,142 +133,157 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local sumneko_root_path = vim.fn.getenv('HOME') .. '/Code/shoukoo/lua-language-server'
-local sumneko_binary = sumneko_root_path .. '/bin/macOS/lua-language-server'
+local sumneko_root_path = vim.fn.getenv("HOME") .. "/Code/shoukoo/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/macOS/lua-language-server"
 
 -- Make runtime files discoverable to the server
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
 
 nvim_lsp.sumneko_lua.setup({
-  cmd = { sumneko_binary, '-E', sumneko_root_path .. '/main.lua' },
-  on_attach = on_attach('sumneko_lua'),
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        path = runtime_path,
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-        },
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+	on_attach = on_attach("sumneko_lua"),
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				path = runtime_path,
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
 
 ---------------------------------------------------------------------
 -- Tree sitter
 ---------------------------------------------------------------------
-require('nvim-treesitter.configs').setup({
-  highlight = {
-    enable = true, -- false will disable the whole extension
-  },
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true, -- false will disable the whole extension
+	},
 })
 
 ---------------------------------------------------------------------
 -- Telescope
 ---------------------------------------------------------------------
 -- Recent visited files
-vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader><space>",
+	[[<cmd>lua require('telescope.builtin').buffers()<CR>]],
+	{ noremap = true, silent = true }
+)
 -- Find files
-vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>ff",
+	[[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]],
+	{ noremap = true, silent = true }
+)
 -- Call RG to grep files
-vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fg",
+	[[<cmd>lua require('telescope.builtin').live_grep()<CR>]],
+	{ noremap = true, silent = true }
+)
 
 ---------------------------------------------------------------------
 -- Comp
 ---------------------------------------------------------------------
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noinsert'
+vim.o.completeopt = "menuone,noinsert"
 
 -- Compe setup
-require('compe').setup({
-  source = {
-    path = true,
-    nvim_lsp = true,
-    luasnip = true,
-    orgmode = true,
-    buffer = false,
-    calc = false,
-    vsnip = false,
-    ultisnips = false,
-  },
+require("compe").setup({
+	source = {
+		path = true,
+		nvim_lsp = true,
+		luasnip = true,
+		orgmode = true,
+		buffer = false,
+		calc = false,
+		vsnip = false,
+		ultisnips = false,
+	},
 })
 
-local luasnip = require('luasnip')
+local luasnip = require("luasnip")
 
 -- Utility functions for compe and luasnip
 local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 local check_back_space = function()
-  local col = vim.fn.col('.') - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-    return true
-  else
-    return false
-  end
+	local col = vim.fn.col(".") - 1
+	if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+		return true
+	else
+		return false
+	end
 end
 
 _G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t('<C-n>')
-  elseif luasnip.expand_or_jumpable() then
-    return t('<Plug>luasnip-expand-or-jump')
-  elseif check_back_space() then
-    return t('<Tab>')
-  else
-    return vim.fn['compe#complete']()
-  end
+	if vim.fn.pumvisible() == 1 then
+		return t("<C-n>")
+	elseif luasnip.expand_or_jumpable() then
+		return t("<Plug>luasnip-expand-or-jump")
+	elseif check_back_space() then
+		return t("<Tab>")
+	else
+		return vim.fn["compe#complete"]()
+	end
 end
 
 _G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t('<C-p>')
-  elseif luasnip.jumpable(-1) then
-    return t('<Plug>luasnip-jump-prev')
-  else
-    return t('<S-Tab>')
-  end
+	if vim.fn.pumvisible() == 1 then
+		return t("<C-p>")
+	elseif luasnip.jumpable(-1) then
+		return t("<Plug>luasnip-jump-prev")
+	else
+		return t("<S-Tab>")
+	end
 end
 
 -- Map tab to the above tab complete functiones
-vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
 
 -- Map compe confirm and complete functions
-vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
-vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
+vim.api.nvim_set_keymap("i", "<cr>", 'compe#confirm("<cr>")', { expr = true })
+vim.api.nvim_set_keymap("i", "<c-space>", "compe#complete()", { expr = true })
 
 ---------------------------------------------------------------------
 -- Orgmode
 ---------------------------------------------------------------------
 local Orgmod_path = [[~/Library/Mobile\ Documents/com~apple~CloudDocs/vim/orgmode/]]
-require('orgmode').setup({
-  org_agenda_files = { Orgmod_path .. '*' },
-  org_default_notes_file = Orgmod_path .. 'default.org',
-  org_agenda_templates = {
-    t = { description = 'default', template = '* TODO %?\n  %u' },
-    n = { description = 'neovim', template = '* TODO %?\n %u', target = Orgmod_path .. 'nvim.org' },
-  },
+require("orgmode").setup({
+	org_agenda_files = { Orgmod_path .. "*" },
+	org_default_notes_file = Orgmod_path .. "default.org",
+	org_agenda_templates = {
+		t = { description = "default", template = "* TODO %?\n  %u" },
+		n = { description = "neovim", template = "* TODO %?\n %u", target = Orgmod_path .. "nvim.org" },
+	},
 })
 
 ---------------------------------------------------------------------
@@ -281,35 +292,35 @@ require('orgmode').setup({
 
 -- Printout values
 _G.Dump = function(tbl)
-  if type(tbl) == 'table' then
-    local s = '{ '
-    for k, v in pairs(tbl) do
-      if type(k) ~= 'number' then
-        k = '"' .. k .. '"'
-      end
-      s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-    end
-    return s .. '} '
-  else
-    return tostring(tbl)
-  end
+	if type(tbl) == "table" then
+		local s = "{ "
+		for k, v in pairs(tbl) do
+			if type(k) ~= "number" then
+				k = '"' .. k .. '"'
+			end
+			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+		end
+		return s .. "} "
+	else
+		return tostring(tbl)
+	end
 end
 
 -- custom handler to call both goimports + gofmt.
 -- vim.lsp.buf.formatting doesn't trigger goimports
 Gopls = function(timeoutms)
-  local context = { source = { organizeImports = true } }
-  local params = vim.lsp.util.make_range_params()
-  params.context = context
+	local context = { source = { organizeImports = true } }
+	local params = vim.lsp.util.make_range_params()
+	params.context = context
 
-  local method = 'textDocument/codeAction'
-  local resp = vim.lsp.buf_request_sync(0, method, params, timeoutms)
-  if resp and resp[1] then
-    local result = resp[1].result
-    if result and result[1] then
-      local edit = result[1].edit
-      vim.lsp.util.apply_workspace_edit(edit)
-    end
-  end
-  vim.lsp.buf.formatting_sync()
+	local method = "textDocument/codeAction"
+	local resp = vim.lsp.buf_request_sync(0, method, params, timeoutms)
+	if resp and resp[1] then
+		local result = resp[1].result
+		if result and result[1] then
+			local edit = result[1].edit
+			vim.lsp.util.apply_workspace_edit(edit)
+		end
+	end
+	vim.lsp.buf.formatting_sync()
 end
