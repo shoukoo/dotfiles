8a9ef042daff7ec823b4f62d7b157cfa2cca55fb
Andy Hsieh <shoukoo.koike@gmail.com>
2022-07-12 23:07:05 +1000

    Update fish prompt to include aws profile and k8s ctx

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,4 @@
 function fish_prompt
-     echo -n -s (set_color $fish_color_cwd --bold) (prompt_pwd)(set_color normal) (fish_git_prompt) " "
+     echo -n -s (set_color fcba03) $AWS_PROFILE (set_color 8cb0ff) "|" (kubectl config current-context) "|" (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (fish_git_prompt) " "
 end
+
