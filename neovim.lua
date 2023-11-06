6a7077dd4d4ffa90e4ba9c4b027ec0920a996918
shoukoo <andyhsieh@duck.com>
2023-11-07 07:09:38 +1100

    feature: use fzf native in telescope

--- neovim.lua
+++ neovim.lua
@@ -76,6 +76,7 @@ require("lazy").setup({
   },
 
   -- telescope
+  {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   { 'nvim-telescope/telescope-ui-select.nvim' },
   {
     "nvim-telescope/telescope.nvim",
@@ -84,9 +85,17 @@ require("lazy").setup({
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
     },
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    },
     config = function()
       require("telescope").setup()
       require("telescope").load_extension("ui-select")
+      require('telescope').load_extension('fzf')
     end,
   },
 
