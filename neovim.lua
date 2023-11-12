a76963cc6a68aee38b66fac8b277307b94e3dce9
Andy <andy.hsieh@anz.com>
2023-11-13 09:22:33 +1100

    fix: some cmd

--- neovim.lua
+++ neovim.lua
@@ -407,7 +407,7 @@ vim.keymap.set('x', '<leader>go', ":<C-u> lua require('git.browse').open(true)<C
 ---------------------------------------------------------------------
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {})
 vim.keymap.set('n', '<leader>fd', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>xx', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
